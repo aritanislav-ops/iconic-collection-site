d9d2f57338871ddbae299d9a004f4ad3ecd2ab73
aritanislav-ops <ari.tanislav@gmail.com>
2026-02-25 14:37:20 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
               </h1>
 
               <p className="heroSub">
-                Arh + Structură + Instalații + autorizare. Execuție opțional.
+                Arh + Structură + Instalații + autorizare. Execuție.
               </p>
 
               <ul className="heroChecks">
